0512b01db0a0339a5b623f19be6338422c03a813
murphyqwek <113290803+murphyqwek@users.noreply.github.com>
2023-03-02 11:31:14 +0700

    Update 26.py

--- 2 полугодие/26.py
+++ 2 полугодие/26.py
@@ -1,22 +1,19 @@
 a = set()
-max_size = -1
-max_min = -1
+max_size = minimum = -1
 with open("26.txt") as f:
     for i in range(int(f.readline())):
-        a.add(int(f.readline()))
-
+        k = int(f.readline())
+        a.add(k)
 a = sorted(list(a))
-
+temp = 0
 for i in range(len(a)):
-    temp = 0
-    last_el = a[i]
+    last_element = a[i]
     for j in range(i+1, len(a) ):
-        if last_el + 3 <= a[j]:
+        if last_element + 3 <= a[j]:
             temp += 1
-            last_el = a[j]
+            last_element = a[j]
     temp += 1
     if max_size <= temp:
         max_size = temp
-        max_min = max(max_min, a[i])
-
-print(max_size, max_min)
+        minimum = max(minimum , a[i])
+print(max_size, minimum)
